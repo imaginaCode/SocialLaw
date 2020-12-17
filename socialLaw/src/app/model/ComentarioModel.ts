43f903bd0ecd669719227b294a62f0576f1e34ea
Natália Moreno <natalialopesmoreno@gmail.com>
2020-12-17 14:24:24 -0300

    Comentarios

--- socialLaw/src/app/model/ComentarioModel.ts
+++ socialLaw/src/app/model/ComentarioModel.ts
@@ -1,8 +1,9 @@
+import { PostagemModel } from "./PostagemModel"
 
 export class ComentarioModel {
-    public id_comentario!: number    
+    public id_comentario!: number
     public usuario!: string
-    public postagem!: string
+    public postagem!: PostagemModel
     public artigo!: string
     public data!: Date
     public curtir!: string
